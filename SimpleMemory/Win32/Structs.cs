c6b3d159f86f520bee386897f2ad3f700b412404
tristan_tr <tristan_tr>
2020-10-21 14:22:37 +0200

    Split Win32 class into seperate files

---
+++ SimpleMemory/Win32/Structs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SimpleMemory
+{
+    internal partial class Win32
+    {
+        internal class Structs
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct MemoryBasicInformation
+            {
+                public IntPtr BaseAddress;
+                public IntPtr AllocationBase;
+                public Enums.MemoryProtection AllocationProtect;
+                public IntPtr RegionSize;
+                public Enums.MemState State;
+                public Enums.MemoryProtection Protect;
+                public Enums.MemType Type;
+            }
+        }
+    }
+}
---
+++ SimpleMemory/Win32/Win32.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace SimpleMemory
+{
+    internal partial class Win32
+    {
+        public const int ERROR_SUCCESS = 0;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern IntPtr OpenProcess(Enums.ProcessAccessFlags processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+#pragma warning disable CS0618 // Type or member is obsolete
+        internal static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [MarshalAs(UnmanagedType.AsAny)] object lpBuffer, int dwSize, out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out, MarshalAs(UnmanagedType.AsAny)] object lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern int VirtualQueryEx(IntPtr hProcess, IntPtr lpBaseAddress, out Structs.MemoryBasicInformation lpBuffer, int dwLength);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpBaseAddress, int dwSize, Enums.MemoryProtection flNewProtect, out Enums.MemoryProtection lpflOldProtect);
+    }
+}
